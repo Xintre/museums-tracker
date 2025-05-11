d58ad71effd356cd0885a10339e8d8db9719c05a
Xintre <natalia.rozga02@gmail.com>
2025-05-11 21:58:49 +0200

    fix: better display of addresses

--- shared/src/dto/nominatim/AddressInfo.ts
+++ shared/src/dto/nominatim/AddressInfo.ts
@@ -1,10 +1,10 @@
 export type AddressInfo = {
 	tourism: string;
-	house_number: string;
+	house_number?: string;
 	road: string;
 	quarter: string;
 	suburb: string;
-	city: string;
+	city?: string;
 	state: string;
 	'ISO3166-2-lvl4': string;
 	postcode: string;
